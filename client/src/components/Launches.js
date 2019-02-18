812cd2c95e115b26ac6e36baa41a284402c4c098
Waga Odongo <wagaodongo@gmail.com>
2019-02-18 13:59:33 +0300

    launches
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,50 +1,43 @@
 import React, { Component, Fragment } from 'react';
 import gql from 'graphql-tag';
-import {Query} from 'react-apollo';
-import LaunchItems from './LaunchItems';
+import { Query } from 'react-apollo';
+import LaunchItem from './LaunchItem';
 import MissionKey from './MissionKey';
 
-
-const LAUNCHES_QUERY =gql`
-query LaunchesQuery {
+const LAUNCHES_QUERY = gql`
+  query LaunchesQuery {
     launches {
-        flight_number
-        mission_name
-        launch_date_local
-        launch_success
+      flight_number
+      mission_name
+      launch_date_local
+      launch_success
     }
-}
+  }
 `;
 
-export class Launches extends Component {
+class Launches extends Component {
   render() {
     return (
       <Fragment>
-          <h1 className="display-4 my-3"> Launches</h1>
-          <MissionKey />
-          <Query query= {LAUNCHES_QUERY} >
-          {
-              ({loading,error,data}) => {
-                  
-                  if(loading) return (<h4> Loading...</h4>)
-                  if(error) console.log(error);
-                  
-                  return <Fragment>
-                      {
-                          data.launches.map(launch =>(
-                              <LaunchItems key={
-                                  launch.flight_number
-                              } launch={launch}/>
-                          ))
-                      }
-                  </Fragment>
-                }
-            }
-          </Query>
-        </Fragment>
-     
-    )
+        <h1 className="display-4 my-3">Launches</h1>
+        <MissionKey />
+        <Query query={LAUNCHES_QUERY}>
+          {({ loading, error, data }) => {
+            if (loading) return <h4>Loading...</h4>;
+            if (error) console.log(error);
+
+            return (
+              <Fragment>
+                {data.launches.map(launch => (
+                  <LaunchItem key={launch.flight_number} launch={launch} />
+                ))}
+              </Fragment>
+            );
+          }}
+        </Query>
+      </Fragment>
+    );
   }
 }
 
-export default Launches
+export default Launches;
