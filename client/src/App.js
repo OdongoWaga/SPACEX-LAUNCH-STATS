f55d24a59144931409c5045ed5f676022e5b092d
Waga Odongo <wagaodongo@gmail.com>
2019-02-18 12:56:14 +0300

    launch
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import ApolloClient from 'apollo-boost';
 import {ApolloProvider} from 'react-apollo'
 import Launches from './components/Launches';
+import Launch from './components/Launch';
 
 
 const client = new ApolloClient({
@@ -15,15 +16,17 @@ class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-      
+        <Router>
       <div className="container">
       <img
       src={logo}
       alt="SpaceX"
       style={{width:300, display:'block', margin:"auto" }}
       />
-      <Launches />
+      <Route exact path="/" component={Launches} /> 
+      <Route exact path="/launch/:flight_number" component={Launch} /> 
       </div>
+      </Router>
       </ApolloProvider>
     );
   }
