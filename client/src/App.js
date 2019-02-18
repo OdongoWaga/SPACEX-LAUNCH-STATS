24024463a8d8adfff845959181d563d4039e3a3d
Waga Odongo <wagaodongo@gmail.com>
2019-02-18 14:17:51 +0300

    app done
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -7,26 +7,25 @@ import {ApolloProvider} from 'react-apollo'
 import Launches from './components/Launches';
 import Launch from './components/Launch';
 
-
 const client = new ApolloClient({
   uri: '/graphql'
-})
+});
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
         <Router>
-      <div className="container">
-      <img
-      src={logo}
-      alt="SpaceX"
-      style={{width:300, display:'block', margin:"auto" }}
-      />
-      <Route exact path="/" component={Launches} /> 
-      <Route exact path="/launch/:flight_number" component={Launch} /> 
-      </div>
-      </Router>
+          <div className="container">
+            <img
+              src={logo}
+              alt="SpaceX"
+              style={{ width: 300, display: 'block', margin: 'auto' }}
+            />
+            <Route exact path="/" component={Launches} />
+            <Route exact path="/launch/:flight_number" component={Launch} />
+          </div>
+        </Router>
       </ApolloProvider>
     );
   }
