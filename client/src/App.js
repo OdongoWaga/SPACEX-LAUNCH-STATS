e4144c21e038f2984a4d486bdb21bf74786107ee
Waga Odongo <wagaodongo@gmail.com>
2019-02-18 10:58:35 +0300

    great
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import logo from './spaceX.jpg'
 import './App.css';
-import { Z_BLOCK } from 'zlib';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo'
+import Launches from './components/Launches';
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <ApolloProvider client={client}>
+      
+      <div className="container">
       <img
       src={logo}
       alt="SpaceX"
       style={{width:300, display:'block', margin:"auto" }}
       />
+      <Launches />
       </div>
+      </ApolloProvider>
     );
   }
 }
