4c96776bcbd9b0716eacbe68e06af188b8218bc8
Waga Odongo <wagaodongo@gmail.com>
2019-02-18 11:07:04 +0300

    launches
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -18,6 +18,17 @@ export class Launches extends Component {
     return (
       <div>
           <h1 className="display-4 my-3"> Launches</h1>
+          <Query query= {LAUNCHES_QUERY} >
+          {
+              ({loading,error,data}) => {
+
+                if(loading) return (<h4> Loading...</h4>)
+                if(error) console.log(error);
+
+                return <h1>test</h1>
+              }
+          }
+          </Query>
         
       </div>
     )
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const graphqlHTTP= require('express-graphql'); 
 const schema = require('./schema');
+const cors = require('cors')
 
 const app = express();
+
+// Allow Cross Origin
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
