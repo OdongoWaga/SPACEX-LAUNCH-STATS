9ea7809cb9cb0ec06b8efc6d025a89d540665f6a
Waga Odongo <wagaodongo@gmail.com>
2019-02-18 12:00:15 +0300

    launch Items
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import gql from 'graphql-tag';
 import {Query} from 'react-apollo';
+import LaunchItems from './LaunchItems';
 
 const LAUNCHES_QUERY =gql`
 query LaunchesQuery {
@@ -16,21 +17,29 @@ query LaunchesQuery {
 export class Launches extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
           <h1 className="display-4 my-3"> Launches</h1>
           <Query query= {LAUNCHES_QUERY} >
           {
               ({loading,error,data}) => {
-
-                if(loading) return (<h4> Loading...</h4>)
-                if(error) console.log(error);
-
-                return <h1>test</h1>
-              }
-          }
+                  
+                  if(loading) return (<h4> Loading...</h4>)
+                  if(error) console.log(error);
+                  
+                  return <Fragment>
+                      {
+                          data.launches.map(launch =>(
+                              <LaunchItems key={
+                                  launch.flight_number
+                              } launch={launch}/>
+                          ))
+                      }
+                  </Fragment>
+                }
+            }
           </Query>
-        
-      </div>
+        </Fragment>
+     
     )
   }
 }
